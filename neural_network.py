9beb65f35e720877453c1fe3717fcc7cc77b9c6a
zenev <nevan.wichers@gmail.com>
2016-12-03 14:17:27 -0800

    2 neurons with backprop in example. Ready to connect to bigger network.

--- neural_network.py
+++ neural_network.py
@@ -1,19 +1,47 @@
 import math
 
-class Neuron:
-    def __init__(self, input_neurons, learning_rate):
+class Node:
+    def get_average_out_gradient(self):
+        ave_out_gradient = 1
+        if len(self.output_nodes) > 0:
+            out_gradients = [self.output_nodes[i].get_gradient_w(i) for i in range(len(self.output_nodes))]
+            ave_out_gradient = sum(out_gradients) / float(len(out_gradients))
+
+        return ave_out_gradient
+
+    def clear_pre_vals(self):
+        pass
+
+    def update_w(self, error):
+        pass
+
+    def get_all_gradient(self):
+        for i in range(len(self.input_nodes)):
+            self.get_gradient_w(i)
+            self.input_nodes[i].get_all_gradient()
+
+    def update_w_all(self, error):
+        self.update_w(error)
+
+        for input_node in self.input_nodes:
+            input_node.update_w_all(error)
+
+class Neuron(Node):
+    def __init__(self, input_nodes, learning_rate):
         self.weights = []
         self.learning_rate = learning_rate
-        self.input_neurons = input_neurons
+        self.input_nodes = input_nodes
+        self.output_nodes = []
+        self.pre_gradient = []
 
-        for i in range(len(self.input_neurons) + 1):
+        for i in range(len(self.input_nodes)):
             self.weights.append(0)
+            self.input_nodes[i].output_nodes.append(self)
+            self.pre_gradient.append(None)
+
+        self.weights.append(0)
 
         self.pre_predict = None
-
-    @staticmethod
-    def sigmoid(x):
-        return 1/(1 + math.exp(-x))
 
     def predict(self):
         if not self.pre_predict:
@@ -26,49 +54,77 @@ class Neuron:
             for i in range(len(self.weights)):
                 y += x[i] * self.weights[i]
 
-            self.pre_predict = Neuron.sigmoid(y)
+            self.pre_predict = y
 
         return self.pre_predict
 
     def get_input_values(self):
         x = []
-        for neuron in self.input_neurons:
-            x.append(neuron.predict())
+        for node in self.input_nodes:
+            x.append(node.predict())
         x = x + [1]
         return x
 
-    def update_w(self, error, gradients):
-        gradient = self.calc_gradient(error, gradients)
+    def update_w(self, error):
+        gradient = self.get_average_out_gradient()
         x = self.get_input_values()
 
         for i in range(len(self.weights)):
-            update = -gradient * x[i] * self.learning_rate
+            update = -gradient * error * x[i] * self.learning_rate
             self.weights[i] += update
 
-    def calc_gradient(self, error, pre_gradients):
-        y = self.predict()
-        ave_pre_gradient = sum(pre_gradients) / float(len(pre_gradients))
-        return error * (y * (1- y)) * ave_pre_gradient
+    def get_gradient_w(self, index):
+        if self.pre_gradient[index] == None:
+            self.pre_gradient[index] = self.get_average_out_gradient() * self.weights[index]
 
-    def get_gradient_w(self, error, pre_gradients, index):
-        return self.calc_gradient(error, pre_gradients) * self.weights[index]
+        return self.pre_gradient[index]
 
-    def clear_pre_predict(self):
+    def clear_pre_vals(self):
         self.pre_predict = None
+        self.pre_gradient = [None for i in range(len(self.input_nodes))]
 
 
-class ConstNode:
+class SigmoidNode(Node):
+    def __init__(self, input_node):
+        self.input_nodes = [input_node]
+        self.output_nodes = []
+        input_node.output_nodes.append(self)
+
+    @staticmethod
+    def sigmoid(x):
+        return 1/(1 + math.exp(-x))
+
+    def predict(self):
+        return SigmoidNode.sigmoid(self.input_nodes[0].predict())
+
+    def get_gradient_w(self, i):
+        y = self.predict()
+
+        return self.get_average_out_gradient() * (y * (1- y))
+
+
+class ConstNode(Node):
     def __init__(self):
         self.value = 0
+        self.output_nodes = []
+        self.input_nodes = []
 
     def predict(self):
         return self.value
 
-    def clear_pre_predict(self):
-        pass
 
+def clear_pre_vals(nodes):
+    for node in nodes:
+        node.clear_pre_vals()
 
 def set_input_layer(input_layer, values):
     for i in range(len(input_layer)):
         input_layer[i].value = values[i]
+
+def optimzer(root_node, y_):
+    nodes = [root_node]
+    
+    while len(nodes) > 0:
+        for node in nodes:
+            new_nodes.extend()
 
--- neural_network_example.py
+++ neural_network_example.py
@@ -7,10 +7,11 @@ def get_accuracy(test_data):
     for example in test_data:
         x = example[:-1]
         y_ = example[-1]
-        neural_network.set_input_layer(input_layer, x)
 
-        y = neuron.predict()
-        neuron.clear_pre_predict()
+        neural_network.set_input_layer(input_layer, x)
+        y = neuron2.predict()
+
+        neural_network.clear_pre_vals([neuron1, neuron2])
 
         pred = int(y > .5)
 
@@ -35,25 +36,32 @@ test_data = data[train_amount:]
 
 input_layer = [neural_network.ConstNode() for i in range(len(train_data[0])-1)]
 
-neuron = neural_network.Neuron(input_layer, .1)
+neuron1 = neural_network.Neuron(input_layer, .1)
+sigmoid1 = neural_network.SigmoidNode(neuron1)
+neuron2 = neural_network.Neuron([sigmoid1], .1)
+sigmoid2 = neural_network.SigmoidNode(neuron2)
 
 start = 0
-print_num = 1000
-for i in range(10000):
+print_num = 10000
+
+neuron2.weights = [1,0]
+for i in range(100000):
     example = train_data[start]
 
     x = example[:-1]
     y_ = example[-1]
 
     neural_network.set_input_layer(input_layer, x)
-    y = neuron.predict()
+    y = neuron2.predict()
     error = y - y_
-    neuron.update_w(error, [1])
 
-    neuron.clear_pre_predict()
+    neuron2.get_all_gradient()
+    neuron2.update_w_all(error)
+
+    neural_network.clear_pre_vals([neuron1, neuron2])
 
     if i % print_num == 0:
-        print("error", y - y_, "x", x, "y_", y_, "y:", y, "w", neuron.weights)
+        print("error", y - y_, "x", x, "y_", y_, "y:", y, "w", neuron1.weights, neuron2.weights)
         print(get_accuracy(test_data))
 
     start += 1
